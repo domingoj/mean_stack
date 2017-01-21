0d1fbcab7d80820aa4232a2b835b48daa0a9d489
jermiedomingo <8jermiedomingo@gmail.com>
2017-01-21 22:45:45 +0400

    express introductory

--- chatcat/server.js
+++ chatcat/server.js
@@ -1,3 +1,16 @@
 'use strict';
+const express = require('express');
+const app = express();
 
-console.log('Welcome to ChatCat');
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res, next) => {
+	res.send('<h1>Hello Express!</h1>');
+});
+
+app.get('/dashboard', (req, res, next) => {
+	res.send('<h1>This is the dashboard page!</h1>');
+
+});
+
+app.listen(app.get('port'), () => console.log('ChatCAT running on port: ', 3000));
