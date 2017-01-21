3d4861d4c3251644c890260a1ed8512e4132fc9a
jermiedomingo <8jermiedomingo@gmail.com>
2017-01-21 22:52:50 +0400

    middleware introductory

--- chatcat/server.js
+++ chatcat/server.js
@@ -4,12 +4,26 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+let helloMiddleware = (req, res, next) => {
+	req.hello = "Hello! Its me...";
+	next();
+}
+
+//plugs the middleware function in
+app.use(helloMiddleware);
+
+
+//if made like this, middleware will only work on routes origination from /dashboard
+// and forward e.g. (/dashboard/password, etc.) 
+//app.use('/dashboard', helloMiddleware);
+
 app.get('/', (req, res, next) => {
 	res.send('<h1>Hello Express!</h1>');
+	console.log(req.hello);
 });
 
 app.get('/dashboard', (req, res, next) => {
-	res.send('<h1>This is the dashboard page!</h1>');
+	res.send('<h1>This is the dashboard page! Middelware says ' + req.hello + '</h1>');
 
 });
 
