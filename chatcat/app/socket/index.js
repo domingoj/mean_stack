e2741991300584369e3d2debb99d559c7e0571a1
jermiedomingo <8jermiedomingo@gmail.com>
2017-01-30 22:19:46 +0400

    Adding new rooms, client server socket communication

--- chatcat/app/socket/index.js
+++ chatcat/app/socket/index.js
@@ -1,21 +1,29 @@
 'use strict';
+const crypto = require('crypto');
 
 module.exports = (io, app) => {
 
 	let allrooms = app.locals.chatrooms;
 
-	//dummy data for testing
-	allrooms.push({
-		room: 'Good Food',
-		roomID: '0001',
-		users: []
-	});
+	let findRoomByName = (allrooms, room) => {
+				let findRoom = allrooms.findIndex((element, index, array) => {
+				
+					if(element.room === room) {
+						return true;
+					} else {
+					return false;
+					}
 
-	allrooms.push({
-		room: 'Travel',
-		roomID: '0002',
-		users: []
-	});
+				});
+
+				return findRoom > -1 ? true : false;
+			}
+
+	//unique roomID generator
+	let randomHex = () => {
+		return crypto.randomBytes(24).toString('hex');
+	}
+
 
 	io.of('/roomslist').on('connection', socket => {
 		
@@ -24,6 +32,27 @@ module.exports = (io, app) => {
 
 			//emit a chatRoomsList event along with the roomslist data
 			socket.emit('chatRoomsList', JSON.stringify(allrooms));
+		});
+
+		socket.on('createNewRoom', newRoomInput => {
+
+			// check to see if a room with the same title exists or not
+			// if not, create one and broadcast it to everyone
+			if(!findRoomByName(allrooms, newRoomInput)){
+
+
+				allrooms.push({
+					room: newRoomInput,
+					roomID: randomHex(),
+					users: []
+				});
+
+				//Emit an updated list to the creator
+				socket.emit('chatRoomsList', JSON.stringify(allrooms));
+				//Emit an updated list to everyone else on the rooms page
+				socket.broadcast.emit('chatRoomsList', JSON.stringify(allrooms));
+			}
+
 		});
 	});
 
